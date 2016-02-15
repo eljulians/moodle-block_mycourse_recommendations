e6cfea80c41e31766ceb1d8248caca7e3c208486
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-02-15 17:52:46 +0100

    dev-course_filter: create main method of "course_filter" class
    Created methods as static, since there's no need to have an instance of the class.

--- classes/course_filter/course_filter.php
+++ classes/course_filter/course_filter.php
@@ -23,49 +23,111 @@
 
 namespace block_mycourse_recommendations;
 
+require_once($CFG->dirroot . '/blocks/mycourse_recommendations/classes/db/database_helper.php');
+
+use block_mycourse_recommendations\database_helper;
+
 defined('MOODLE_INTERNAL') || die();
 
 class course_filter {
 
-    private $minimumweeks;
-    private $minimumpreviouscourses;
-    private $slidingfactor;
-    private $minimummodules;
-    private $minimumpreviousstudents;
-    private $modulevariationpercentage;
+    private static $minimumweeks = 4;
+    private static $minimumpreviouscourses = 1;
+    private static $minimumpreviousstudents = 20;
+    private static $minimumpreviousmodules = self::$minimumpreviousstudents * 2;
 
+    /**
+     * Determines if a course is personalizable or not. The logic to determine this is divided in other
+     * functions; this function only calls them. If one of these function says that doesn't meet one of
+     * the parameters, there's no need to continue calling other functions.
+     *
+     * @see meets_minimum_weeks($courseid, $db).
+     * @see meets_minimum_previous_courses($courseid, $db).
+     * @see meets_minimum_resources($courseid, $db).
+     * @see meets_minimum_previous_students($courseid, $db).
+     * @see meets_resource_variation($courseid, $db).
+     * @param int $courseid The course to determine if it is personalizable or not.
+     * @return boolean If the given course is personalizable or not.
+     */
+    public static function is_course_personalizable($courseid) {
+        $db = new database_helper();
 
-    public function __construct() {
+        $personalizable = true;
+
+        $personalizable .= self::meets_minimum_weeks($courseid, $db);
+        if ($personalizable) {
+            $personalizable .= self::meets_minimum_previous_courses($courseid, $db);
+        }
+
+        if ($personalizable) {
+            $personalizable .= self::meets_minimum_resources($courseid, $db);
+        }
+
+        if ($personalizable) {
+            $personalizable .= self::meets_minimum_previous_students($courseid, $db);
+        }
+
+        if ($personalizable) {
+            $personalizable .= self::meets_resource_variation($courseid, $db);
+        }
+
+        return $personalizable;
+    }
+
+    /**
+     * Determines if the course duration, in weeks, is long enough to consider it personalizable.
+     *
+     * @param int $courseid The course to determine if meets the minimum weeks.
+     * @param database_helper $db The object with deals with database.
+     * @return boolean If the duration of the given course is the needed.
+     */
+    public static function meets_minimum_weeks($courseid, $db) {
 
     }
 
     /**
-     * Determines if a course is personalizable or not.
+     * Determines if the course has had the minimum previous teachings in the past years.
      *
-     * @param int $courseid The course to determine if it is personalizable or not.
+     * @param int $courseid The course to determine if has had the minimum teachings before.
+     * @param database_helper $db The object with deals with database.
+     * @return boolean If the given course has had the minimum teachings before or not.
      */
-    public function is_course_personalizable($courseid) {
+    public static function meets_minimum_previous_courses($courseid, $db) {
 
     }
 
-    public function meets_minimum_weeks($courseid) {
+    /**
+     * Determines if the course has the minimum number of resources.
+     *
+     * @param int $courseid The course to determine if meets the minimum modules.
+     * @param database_helper $db The object with deals with database.
+     * @return boolean If the given course has the minimum resources or not.
+     */
+    public static function meets_minimum_resources($courseid, $db) {
 
     }
 
-    public function meets_minimum_previous_courses($courseid) {
+    /**
+     * Determines if the course has had the minimum number of students in previous teachings.
+     *
+     * @param int $courseid The course to determine if meets the minimum students.
+     * @param database_helper $db The object with deals with database.
+     * @return boolean If the given course has the minimum students or not.
+     */
+    public static function meets_minimum_previous_students($courseid, $db) {
 
     }
 
-    public function meets_minimum_modules($courseid) {
-
-    }
-
-    public function meets_minimum_previous_students($courseid) {
-
-    }
-
-    public function meets_module_variation($courseid) {
-
+    /**
+     * Determines if the course meets the resource variation.
+     * For the moment, we'll suppose every course meets this.
+     *
+     * @param int $courseid The course to determine 
+     * @param database_helper $db The object with deals with database.
+     * @return boolean If the given course meets the resource variation or not.
+     */
+    public static function meets_resource_variation($courseid, $db) {
+        return true;
     }
 
 }
