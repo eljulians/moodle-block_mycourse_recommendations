36c626bd25d9c1f6ffed44c5ebd72af8ca2476b2
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-02-16 16:37:30 +0100

    dev-course_filter: refactor tests, make code cleare, add comments, remove unused things

--- tests/course_filter_test.php
+++ tests/course_filter_test.php
@@ -41,7 +41,6 @@ class block_mycourse_recommendations_course_filter_testcase extends advanced_tes
     protected $db;
     protected $previouscourseattributes;
     protected $previouscourses;
-    protected $previousresources;
     protected $currentyear;
     protected $currentcourseattributes;
     protected $currentcourse;
@@ -59,17 +58,12 @@ class block_mycourse_recommendations_course_filter_testcase extends advanced_tes
                                                'startdate' => strtotime("15-02-$this->currentyear"));
 
         $this->currentcourse = $this->create_courses($this->currentcourseattributes, 1);
-
-        // $this->previouscourses = $this->create_courses($this->previouscourseattributes, 3);
-
-        //$this->create_and_enrol_students($this->previouscourses[0]->id, course_filter::MINIMUM_PREVIOUS_STUDENTS);
     }
 
     protected function tearDown() {
         $this->db = null;
         $this->previouscourseattributes = null;
         $this->previouscourses = null;
-        $this->previousresources = null;
         $this->currentyear = null;
         $this->currentcourseattributes = null;
         $this->currentcourse = null;
@@ -123,196 +117,262 @@ class block_mycourse_recommendations_course_filter_testcase extends advanced_tes
         }
     }
 
+    // Tests for "meets_minimum_previous_courses" function start here.
+
     /**
-     * Tests that the function returns false when the number of previous courses is lower than the minimum
-     * defined.
+     * Tests that the function returns false when the number of previous courses is lower than the minimum defined,
+     * i.e., the input is OUTSIDE the lower limit of the input domain.
      */
     public function test_meets_minimum_previous_courses_below() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course won't meet the minimum previous courses, so, we expect a false from the function.
         $expected = false;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES - 1; // As said above, we create less than the minimum.
+        // We stablish a value that is outside, in the lower limit, of the input domain.
+        $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES - 1;
 
         // We create those courses, with the attributes defined in setUp.
         $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;        
         $actual = course_filter::meets_minimum_previous_courses($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
     /**
-     * Tests that the function returns true when the number of previous courses is the same as the minimum defined.
+     * Tests that the function returns true when the number of previous courses is the same as the minimum defined,
+     * i.e., the input is EQUAL to the lower limit (the unique limit, in this case) of the input domain.
      */
     public function test_meets_minimum_previous_courses_equal() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous courses, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
+        // We stablish the value that defines the lower limit (and the unique one) of the input domain.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
 
         // We create those courses, with the attributes defined in setUp.
         $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_previous_courses($currentcourseid, $this->currentyear, $this->db);
 
-        $this->assertEquals($expected, $actual);   
+        $this->assertEquals($expected, $actual);
     }
 
     /**
-     * Tests that the function returns true when the number of previous courses is higher than the minumimum defined.
+     * Tests that the function returns true when the number of previous courses is higher than the minumimum defined,
+     * i.e., the input is ABOVE the lower limit of the input domain. In this case, since the input domain only has a limit,
+     * the lower one, the input is also INSIDE.
      */
     public function test_meets_minimum_previous_courses_above() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous courses, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
+        // We stablish a value that is inside the input domain.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES + 1;
 
         // We create those courses, with the attributes defined in setUp.
         $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_previous_courses($currentcourseid, $this->currentyear, $this->db);
 
-        $this->assertEquals($expected, $actual);   
+        $this->assertEquals($expected, $actual);
     }
 
+    // Tests for "meets_minimum_resources" start here.
+
+    /**
+     * Tests that the function returns false when the number of previous resources is lower than the minimum defined,
+     * i.e., the input is OUTSIDE the lower limit of the input domain.
+     */
     public function test_meets_minimum_resources_below() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course won't meet the minimum previous resources, so, we expect a true from the function.
         $expected = false;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to create some courses before creating resources...
+        // We stablish a value that is outside, in the lower limit, of the input domain.
+        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES - 1;
+
+        // We create those courses, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        // We set the total number of resources to create.
-        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES - 1;
+        // We divide the resources to create in, let's say, 3 types of resources, to have a bit of variation, and we create them.
         $previousresources = array();
-
-        // We divide the resources to create in, let's say, 3 types of resources, to have a bit of variation.
         $previousresources[$previouscourses[0]->id]['mod_page'] = $numberofresources / 3;
         $previousresources[$previouscourses[0]->id]['mod_url'] = $numberofresources / 3;
         $previousresources[$previouscourses[0]->id]['mod_book'] = $numberofresources / 3;
         $this->create_resources($previousresources);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_resources($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * Tests that the function returns true when the number of previous resources is the same as the minimum defined,
+     * i.e., the input is EQUAL to the lower limit (the unique limit, in this case) of the input domain.
+     */
     public function test_meets_minimum_resources_equal() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous resources, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to creates some courses before creating resources...
+        // We stablish the value that defines the lower limit (and the unique one) of the input domain.
+        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES;
+
+        // We have to creates some courses before creating resources, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        // We set the total number of resources to create.
-        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES;
+        // We divide the resources to create in, let's say, 2 types of resources, to have a bit of variation, and we create them.
         $previousresources = array();
-
-        // We divide the resources to create in, let's say, 2 types of resources, to have a bit of variation.
         $previousresources[$previouscourses[0]->id]['mod_page'] = $numberofresources / 2;
         $previousresources[$previouscourses[0]->id]['mod_url'] = $numberofresources / 2;
         $this->create_resources($previousresources);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_resources($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * Tests that the function returns true when the number of previous resources is higher than the minumimum defined,
+     * i.e., the input is ABOVE the lower limit of the input domain. In this case, since the input domain only has a limit,
+     * the lower one, the input is also INSIDE.
+     */
     public function test_meets_minimum_resources_above() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous resources, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to creates some courses before creating resources...
+        // We stablish a value that is inside the input domain.
+        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES + 1;
+
+        // We have to creates some courses before creating resources, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        // We set the total number of resources to create.
-        $numberofresources = course_filter::MINIMUM_PREVIOUS_RESOURCES + 1;
-        $previousresources = array();
-
         // We divide the resources to create in, let's say, 3 types of resources, to have a bit of variation.
+        $previousresources = array();
         $previousresources[$previouscourses[0]->id]['mod_page'] = $numberofresources / 3;
         $previousresources[$previouscourses[0]->id]['mod_url'] = $numberofresources / 3;
         $previousresources[$previouscourses[0]->id]['mod_book'] = $numberofresources / 3;
         $this->create_resources($previousresources);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_resources($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
+    // Tests for "meets_minimum_previous_students" start here.
+
+    /**
+     * Tests that the function returns false when the number of previous students is lower than the minimum defined,
+     * i.e., the input is OUTSIDE the lower limit of the input domain.
+     */
     public function test_meets_minimum_previous_students_below() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course won't meet the minimum previous students, so, we expect a true from the function.
         $expected = false;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to creates some courses before creating students...
+        // We stablish a value that is outside, in the lower limit, of the input domain.
+        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS - 1;
+
+        // We have to creates some courses before creating students, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS - 1;
+        // We create and enrol the students in one course...
         $this->create_and_enrol_students($previouscourses[0]->id, $numberofstudents);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_previous_students($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * Tests that the function returns true when the number of previous students is the same as the minimum defined,
+     * i.e., the input is EQUAL to the lower limit (the unique limit, in this case) of the input domain.
+     */
     public function test_meets_minimum_previous_students_equal() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous students, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to creates some courses before creating students...
+        // We stablish the value that defines the lower limit (and the unique one) of the input domain.
+        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS;
+
+        // We have to creates some courses before creating students, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS;
+
+        // We create and enrol the students in one course...
         $this->create_and_enrol_students($previouscourses[0]->id, $numberofstudents);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_previous_students($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * Tests that the function returns true when the number of previous students is higher than the minumimum defined,
+     * i.e., the input is ABOVE the lower limit of the input domain. In this case, since the input domain only has a limit,
+     * the lower one, the input is also INSIDE.
+     */
     public function test_meets_minimum_previous_students_above() {
         $this->resetAfterTest();
         $this->setAdminUser();
 
+        // The course will meet the minimum previous students, so, we expect a true from the function.
         $expected = true;
-        $currentcourseid = $this->currentcourse[0]->id;
 
-        // We have to creates some courses before creating students...
+        // We stablish a value that is inside the input domain.
+        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS + 1;
+
+        // We have to creates some courses before creating students, with the attributes defined in setUp.
         $numberofcoursestocreate = course_filter::MINIMUM_PREVIOUS_COURSES;
         $previouscourses = $this->create_courses($this->previouscourseattributes, $numberofcoursestocreate);
 
-        $numberofstudents = course_filter::MINIMUM_PREVIOUS_STUDENTS + 1;
+        // We create and enrol the students in one course...
         $this->create_and_enrol_students($previouscourses[0]->id, $numberofstudents);
 
+        // We get the actual value and we do the assertion.
+        $currentcourseid = $this->currentcourse[0]->id;
         $actual = course_filter::meets_minimum_previous_students($currentcourseid, $this->currentyear, $this->db);
 
         $this->assertEquals($expected, $actual);
