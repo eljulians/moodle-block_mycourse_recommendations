04ce19f901d5e49f7cf903ceaa56f66fffa821b8
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-02-22 21:45:08 +0100

    dev-task: create structure and define schedule for cron job

---
+++ classes/task/create_recommendations_task.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    block_mycourse_recommendations
+ * @copyright  2016 onwards Julen Pardo & Mondragon Unibertsitatea
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_mycourse_recommendations\task;
+
+defined('MOODLE_INTERNAL') || die();
+
+class create_recommendations_task extends \core\task\scheduled_task {
+
+    public function get_name() {
+        return 'My_course recommendations task (hardcoded!)';
+    }
+
+    public function execute() {
+
+    }
+}
