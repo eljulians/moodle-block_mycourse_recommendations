70007da3699c8f84cfa936ad659ad2e81e95a4ee
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-03-03 10:35:27 +0100

    issue #20: code first test for "get_content" function

---
+++ tests/block_mycourse_recommendations_test.php
@@ -0,0 +1,91 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for database handling of mycourse_recommendations block.
+ *
+ * @package    block_mycourse_recommendations
+ * @category   phpunit
+ * @copyright  2016 onwards Julen Pardo & Mondragon Unibertsitatea
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/blocks/moodleblock.class.php');
+require_once($CFG->dirroot . '/blocks/mycourse_recommendations/block_mycourse_recommendations.php');
+require_once($CFG->dirroot . '/blocks/mycourse_recommendations/classes/db/database_helper.php');
+
+use block_mycourse_recommendations\database_helper;
+
+/**
+ * Test cases for block_mycourse_recommendations for block output.
+ */
+class block_mycourse_recommendations_testcase extends advanced_testcase {
+
+    protected $block;
+    protected $db;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->block = new block_mycourse_recommendations();
+        $this->db = new database_helper();
+    }
+
+    protected function tearDown() {
+        $this->db = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Creates a number of courses with the given attributes.
+     *
+     * @param array $attributes The attributes of the course (fullname, startdate, etc.).
+     * @param int $number The number of courses to create for the given previous parameters.
+     * @return array The created courses.
+     */
+    protected function create_courses($attributes, $number) {
+        $courses = array();
+
+        for ($index = 0; $index < $number; $index++) {
+            $courses[$index] = $this->getDataGenerator()->create_course($attributes);
+        }
+
+        return $courses;
+    }
+
+    public function test_get_content_() {
+        global $COURSE;
+
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $expected = get_string('notpersonalizable', 'block_mycourse_recommendations');
+
+        // We create the course...
+        $course = $this->create_courses(array(), 1)[0];
+
+        // We set the global variable that the function uses to get the courseid.
+        $COURSE = $course;
+
+        $this->block->init();
+        $actual = $this->block->get_content();
+
+        $this->assertEquals($expected, $actual);
+    }
+}
